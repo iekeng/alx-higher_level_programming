c049a7a601046939e22f8e1552b4b18471aa6b4c
iekeng <ekeng7.eb@gmail.com>
2023-01-07 06:19:29 -0800

    list integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,6 @@
-#!/usr/bin/python3
+#!/usr/bin/python
+
 def print_list_integer(my_list=[]):
-    for x in my_list:
+    for (x in my_list):
         if isinstance(x, int):
-	    print("{}".format(x))
+            print("{}".format(x))

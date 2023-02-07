163ca4b2f550dc68352863e318af5162942f94f8
iekeng <ekeng7.eb@gmail.com>
2023-02-07 21:00:51 +0100

    workings of public instance methods

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''
+BaseGeometry() has area() and integer_validator public instance method.
+'''
+
+
+class BaseGeometry():
+    '''
+    The public instance method of this class
+    raises an exception error
+    '''
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        '''
+        Checks if 'value' gets an int data type
+
+        Args:
+            name - str
+            value - int
+
+        Raises:
+        TypeError: If value is not an integer
+        ValueError: If value is <= 0
+
+        '''
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))

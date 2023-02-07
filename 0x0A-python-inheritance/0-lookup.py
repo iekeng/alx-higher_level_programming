a32d2afbf4be7396c6c92d1c00702806c554b5c2
iekeng <ekeng7.eb@gmail.com>
2023-02-07 11:01:47 +0100

    initial commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+'''
+The lookup module checks all attributes of an object
+'''
+
+
+def lookup(obj):
+    '''
+    Returns a list of available attributes and methods of obj
+    '''
+    return list(dir(obj))

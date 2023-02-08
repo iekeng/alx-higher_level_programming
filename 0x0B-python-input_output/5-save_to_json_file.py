aee10010ff3e99bb994888499d430f125a71ccf0
iekeng <ekeng7.eb@gmail.com>
2023-02-08 14:56:01 +0100

    overwrite an object to a text file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,7 +5,6 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """writes an object to a text file using JSON"""
-    x = json.dumps(my_obj)
 
     with open(filename, mode='w', encoding="utf-8") as f:
         json.dumps(my_obj, f)

043ba2d8f3f9abfd38a11b1409959768379ed992
iekeng <ekeng7.eb@gmail.com>
2023-02-08 10:26:12 +0100

    reads from a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Defines a .txt file reader"""
+"""Defines a .txt file reading function"""
 
 
 def read_file(filename=""):
     """Prints the files contents to standard output"""
     with open(filename, mode='r', encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")

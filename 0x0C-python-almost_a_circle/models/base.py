176a361d2a7a1fca716f20f96933cff9f0f98014
iekeng <ekeng7.eb@gmail.com>
2023-02-14 15:10:18 +0100

    add base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
+"""
+Defines a base model class
+"""
+
 
 class Base:
+    """
+    Base model for all classes
+
+    Private class Atribute:
+    __nb_objects
+    """
     __nb_objects = 0
+
     def __init__(self, id=None):
-        if not id == None:
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id == Base.__nb_objects

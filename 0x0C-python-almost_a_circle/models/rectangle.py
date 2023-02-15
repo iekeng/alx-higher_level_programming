4a262baa240301fedd8eaa222e7da543ac78b355
iekeng <ekeng7.eb@gmail.com>
2023-02-15 22:24:11 +0100

    making triangles

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,24 +4,20 @@
 Defines a rectangle class
 """
 from models.base import Base
-"""
-Represent a rectangle
-"""
 
 
 class Rectangle(Base):
-    """
-    Initialize a Rectangle
-
-    Args:
-    width(int): width of the rectangle
-    height(int): height of the rectangle
-    x(int): the x coordinate of the rectangle
-    y(int): the y coordinate of the rectangle
-    id(int): rectangle's identity
-
-    """
+    """Represent a rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
+        """ Initialize a Rectangle
+
+        Args:
+        width(int): width of the rectangle
+        height(int): height of the rectangle
+        x(int): the x coordinate of the rectangle
+        y(int): the y coordinate of the rectangle
+        id(int): rectangle's identity
+        """
 
         self.__width = width
         self.__height = height
@@ -29,34 +25,42 @@ class Rectangle(Base):
         self.__y = y
         super().__init__(id)
 
-        @property
-        def width(self):
-            return self.__width
+    @property
+    def width(self):
+        """set width of rectangle"""
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        """Get width of rectangle"""
+        self.__width = value
 
-        @property
-        def height(self):
-            return self.__height
+    @property
+    def height(self):
+        """Set height of rectangle"""
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        """Get height of rectangle"""
+        self.__height = value
 
-        @property
-        def x(self):
-            return self.__x
+    @property
+    def x(self):
+        """Get the height of the x coordinate"""
+        return self.__x
 
-        @x.setter
-        def x(self, value):
-            self.__x = value
+    @x.setter
+    def x(self, value):
+        """Set the value of the x coordinate"""
+        self.__x = value
 
-        @property
-        def y(self):
-            return self.__y
+    @property
+    def y(self):
+        """Get the x coordinate"""
+        return self.__y
 
-        @y.setter
-        def y(self, value):
-            self.__y = value
+    @y.setter
+    def y(self, value):
+        """Set the y coordinate"""
+        self.__y = value

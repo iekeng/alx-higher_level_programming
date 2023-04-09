2484c20ba7a2472fe8755a8116adb28147966d04
iekeng <ekeng7.eb@gmail.com>
2023-04-09 06:27:46 +0100

    creates table and adds multiple rows

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,6 @@
--- Create second_table table in hbtn_0c_0 db.
+-- Create second_table table in hbtn_0c_0 db and add multiple rows
 CREATE TABLE IF NOT EXIST second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table VALUES 
-    ('1', 'John', '10'),
-    ('2', 'Alex', '3'),
-    ('3', 'Bob', '14'),
-    ('4', 'George', '8');
+INSERT INTO second_table VALUES (1, 'John', 10);
+INSERT INTO second_table VALUES (2, 'Alex', 3);
+INSERT INTO second_table VALUES (3, 'Bob', 14);
+INSERT INTO second_table VALUES (4, 'George', 8);

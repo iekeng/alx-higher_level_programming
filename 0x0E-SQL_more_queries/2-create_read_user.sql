457da2273f35905c5bd3f2430f157784b1edcf95
iekeng <ekeng7.eb@gmail.com>
2023-04-10 03:19:52 +0100

    creates user and db

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,5 @@
 -- Create hbtn_0d_2 db and user_0d_2 user
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS user_0d_2
-IDENTIFY BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFY BY 'user_0d_2_pwd';
 -- Grant SELECT privilege to user_0d_2 on hbtn_0d_2 db
-GRANT SELECT ON hbtn_0d_2.*
-TO user_0d_2@localhost;
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;

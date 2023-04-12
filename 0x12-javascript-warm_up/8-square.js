f6b55a1233e40599f9568fefe96f954d13066416
iekeng <ekeng7.eb@gmail.com>
2023-04-12 10:54:55 +0100

    prints a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+if (isNaN(process.argv[2])) {
+  console.log('Missing size')
+} else {
+  const x = parseInt(process.argv[2]);
+  const y = 'X';
+  for (let i = 0; i < x; i++) {
+    console.log(y.repeat(x));
+  }
+}

3c5b566660a6975a3e3bb16a2bc8b1d0e680293d
iekeng <ekeng7.eb@gmail.com>
2023-04-14 03:17:20 +0100

    manipulates object

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,10 +4,9 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-myObject.incr = function (n) {
-  n = this.value;
-  n++;
-}
+myObject.incr = function (this.value) {
+  this.value++;
+};
 console.log(myObject);
 myObject.incr();
 console.log(myObject);

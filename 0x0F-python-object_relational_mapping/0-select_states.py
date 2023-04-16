4418d4b49f17d2bf8f164d1361dfe0fe3327a83f
iekeng <ekeng7.eb@gmail.com>
2023-04-16 06:03:45 +0100

    initial commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,13 @@
+#/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQL.connect(host="localhost", port=3306, username=sys.argv[1], password=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()

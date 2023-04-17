fdc53560bf5c3bcd34ee521a8b1856d599ea0ab8
iekeng <ekeng7.eb@gmail.com>
2023-04-17 07:26:12 +0100

    case sensitive search for state

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,17 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+import MySQLdb
+import sys
+"""Displays all values in the states table where name matches the argument"""
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", passwd=sys.argv[2],
+                         user=sys.argv[1], port=3306, db="hbtn_0e_0_usa")
+    cursor = db.cursor()
+
+    rows = cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 
+                          '{}'".format(sys.argv[4]))
+
+    for row in rows:
+        print(row)
+    
+    cursor.close()
+    db.close()

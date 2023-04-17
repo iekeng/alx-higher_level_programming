aaf1922cb77765b0197449847e954529424951cd
iekeng <ekeng7.eb@gmail.com>
2023-04-17 10:23:45 +0100

    list cities in the db

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,19 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+"""list all the cities of a state"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3],
+                         port=3306)
+    cursor = db.cursor()
+    state_name = sys.argv[4]
+    cursor.execute("""SELECT cities.name
+                   FROM cities
+                   INNER JOIN states
+                   ON states.id = cities.state_id
+                   WHERE state.name = %s""", (state_name,))
+
+    cursor.close()
+    db.close()

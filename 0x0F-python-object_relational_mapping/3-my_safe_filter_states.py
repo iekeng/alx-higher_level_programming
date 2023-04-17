225f190c1f52769c2ef4fd7d13d45bcec1927521
iekeng <ekeng7.eb@gmail.com>
2023-04-17 08:37:49 +0100

    lists all states in db

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,19 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+"""lists all states from the db"""
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    name = sys.argv[4]
+    cursor.execute("""SELECT * FROM states WHERE name = %s""", (name,))
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()

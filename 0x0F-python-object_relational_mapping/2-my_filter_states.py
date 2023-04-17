c157b12c478156aa1711bc3fa3f30021500c89d3
iekeng <ekeng7.eb@gmail.com>
2023-04-17 07:30:25 +0100

    case sensitive search for state

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import MySQLdb
 import sys
-"""Displays all values in the states table where name matches the argument"""
+"""Displays all values in the states table where name matches the argument"""i
+
+
 if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", passwd=sys.argv[2],
                          user=sys.argv[1], port=3306, db="hbtn_0e_0_usa")

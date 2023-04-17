50ff045b0c5edcd82940275c42e7206ec14ba91c
iekeng <ekeng7.eb@gmail.com>
2023-04-17 07:29:13 +0100

    case sensitive search for state

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,11 +7,10 @@ if __name__ == '__main__':
                          user=sys.argv[1], port=3306, db="hbtn_0e_0_usa")
     cursor = db.cursor()
 
-    rows = cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 
+    rows = cursor.execute("SELECT * FROM states WHERE name LIKE BINARY
                           '{}'".format(sys.argv[4]))
 
     for row in rows:
         print(row)
-    
     cursor.close()
     db.close()

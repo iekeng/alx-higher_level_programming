357e3219060035340e9f6024c59bedbca1dde2ee
iekeng <ekeng7.eb@gmail.com>
2023-05-01 13:43:45 +0100

    sends post request

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,18 @@
-#\!/usr/bin/python3
+#!/usr/bin/python3
+"""send post request email data"""
+import sys
+import urllib.request
+import urllib.parse
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value  = {"email" : sys.argv[2]}
+    data = urllib.parse.encode(value)
+    full_data = dat.encode('ascii')
+
+    req = urllib.request.Request(url, full_data)
+    
+    with urllib.request.urlopen(req) as response:
+         print(response.read().decode('utf-8'))
+
+

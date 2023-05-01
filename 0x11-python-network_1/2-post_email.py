def6179fe868e6252333a1d1623d81276f97a26e
iekeng <ekeng7.eb@gmail.com>
2023-05-01 13:48:22 +0100

    sends post request

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,13 +6,11 @@ import urllib.parse
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value  = {"email" : sys.argv[2]}
+    value = {"email": sys.argv[2]}
     data = urllib.parse.encode(value)
-    full_data = dat.encode('ascii')
+    full_data = data.encode("ascii")
 
     req = urllib.request.Request(url, full_data)
-    
+
     with urllib.request.urlopen(req) as response:
-         print(response.read().decode('utf-8'))
-
-
+        print(response.read().decode("utf-8"))

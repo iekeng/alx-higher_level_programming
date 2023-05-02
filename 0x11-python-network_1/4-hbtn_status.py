e122e4d5256db6b0c5980f18c331f91ea0df53db
iekeng <ekeng7.eb@gmail.com>
2023-05-02 23:12:19 +0100

    retrives data from url

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """retrieves data from url"""
+import requests
 
 
 if __name__ == "__main__":
-    import requests
-
     r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))

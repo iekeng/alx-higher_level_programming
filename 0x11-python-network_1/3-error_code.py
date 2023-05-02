96262da4eca8c23fb113bb27bcedf9845e8d6caa
iekeng <ekeng7.eb@gmail.com>
2023-05-02 22:42:43 +0100

    sends url request

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """Sends url request"""
+
+
 if __name__ == "__main__":
     import urllib.request
     import sys
 
     url = sys.argv[1]
+
     try:
         with urllib.request.urlopen(url) as response:
             print(response.read().decode('utf-8'))

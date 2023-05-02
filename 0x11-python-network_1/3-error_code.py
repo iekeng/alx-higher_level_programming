4d44d44b7607c896efb5e607c0465eadd96bf3a9
iekeng <ekeng7.eb@gmail.com>
2023-05-02 22:45:05 +0100

    sends url request

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
 
     try:
         with urllib.request.urlopen(url) as response:
-            print(response.read().decode('utf-8'))
+            print(response.read().decode('UTF-8'))
     except urllib.error.HTTPError as e:
         print('Error code: ', e.code)

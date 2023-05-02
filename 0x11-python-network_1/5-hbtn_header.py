ff06d79c9cc0b422aab73dc6b77ecc44f8e95b3d
iekeng <ekeng7.eb@gmail.com>
2023-05-02 23:51:13 +0100

    get header value

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,11 @@
-#\!/usr/bin/python3
+#!/usr/bin/python3
+"""Get the value of X-Request-Id header"""
+
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    r = requests.get(sys.argv[1])
+    res = r.headers.get('X-Request-Id')
+    print(res)

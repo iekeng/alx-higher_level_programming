35338ecdc41fb1fcf7d95383996f1570d3b90801
iekeng <ekeng7.eb@gmail.com>
2023-05-23 22:01:31 +0100

    write a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 fs.writeFile (process.argv[2], process.argv[3], 'utf-8', (error) => {
     if (error){
-        console.error(err);
+        console.error(error);
         return;
     }
 });

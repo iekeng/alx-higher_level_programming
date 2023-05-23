6e64e46f3f8bb8db7637069783c1d31e0e1270ee
iekeng <ekeng7.eb@gmail.com>
2023-05-23 22:24:19 +0100

    request html

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
+//pipe from url to file
+
 const request = require('request');
 const fs = require('fs');
 const url = process.argv[2];
 const file = process.argv[3];
+
 request.get(url).pipe(fs.createWriteStream(file));

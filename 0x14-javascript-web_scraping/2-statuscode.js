4c82c993edd763f65b26ec16ed332a421df9d089
iekeng <ekeng7.eb@gmail.com>
2023-05-23 20:10:42 +0100

    status check

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,10 +2,10 @@
 
 const request = require('request');
 
-request (process.argv[2], (err, res, bdy) => {
-  if (err){
+request(process.argv[2], (err, res, bdy) => {
+  if (err) {
     console.error(err);
     return;
   }
-  console.log('code: ', res && res.statusCode);
+  console.log('code:', res && res.statusCode);
 });

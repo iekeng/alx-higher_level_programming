cb9ac2f560e26357990d6d52d6f476d967f54da0
iekeng <ekeng7.eb@gmail.com>
2023-05-23 21:31:09 +0100

    statuss code

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,5 +4,8 @@
 const fs = require('fs');
 
 fs.writeFile (process.argv[2], process.argv[3], 'utf-8', (error) => {
-    if (err) console.log(err);
+    if (error){
+        console.error(err);
+        return;
+    }
 });

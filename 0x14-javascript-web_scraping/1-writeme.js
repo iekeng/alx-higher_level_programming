071c62fc504b700d169a1a98950d34684539a70e
Ekeng Iniko <ekeng7.eb@gmail.com>
2023-05-24 12:57:53 +0100

    update

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,6 +2,6 @@
 // writes to file using file system
 
 const fs = require('fs');
-fs.writeFile (process.argv[2], process.argv[3], 'utf-8', (error) => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (error) => {
   if (error) console.error(error);
 });
